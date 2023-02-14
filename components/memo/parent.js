1b6db4abe88f9fa2b234e17a56565d2707b7193a
Mohd Saqib <saqib.m@opensenselabs.com>
2023-02-14 11:59:27 +0530

    Changes

---
+++ components/memo/parent.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Parent = () => {
+    return (
+        <div>Parent</div>
+    )
+}
+export default Parent;
---
+++ pages/memo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MemoExample  = () => {
+  return (
+    <div>MemoExample </div>
+  )
+}
+
+export default MemoExample 
